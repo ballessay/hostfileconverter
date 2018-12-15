7c8e2620aa44dfb8e0c83528dc6404e3587b5e05
ballessay <code@ballessay.de>
2018-12-15 17:36:26 +0100

    Add another ip provider

--- DownloadHostsFiles.sh
+++ DownloadHostsFiles.sh
@@ -4,4 +4,5 @@ wget -O winhelp2002.hosts "http://winhelp2002.mvps.org/hosts.txt"
 wget -O hosts-file.net.hosts "http://hosts-file.net/download/hosts.txt"
 wget -O pgl.yoyo.org.hosts "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"
 wget -O someonewhocares.hosts "http://someonewhocares.org/hosts/hosts"
+wget -O adway.hosts "https://adaway.org/hosts.txt"
 
